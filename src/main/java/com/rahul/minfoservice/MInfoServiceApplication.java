72915140e80a63c7a3cd0d04db00f584cc7ca6b0
Rahul Balivada <rahulvarun.b@gmail.com>
2023-10-05 21:26:51 -0500

    initial

---
+++ src/main/java/com/rahul/minfoservice/MInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.minfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahul/minfoservice/MInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.minfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
